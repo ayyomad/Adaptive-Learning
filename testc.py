cdf0140859882576fdc11fd194646e19c18fdeb3
madhav <sreemadhavoffical4@gmail.com>
2025-02-15 16:47:03 +0530

    Added testc.py

---
+++ testc.py
@@ -0,0 +1,14 @@
+import os
+import json
+import random
+
+from bs4 import BeautifulSoup
+import requests
+
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+import matplotlib.pyplot as plt
+
+
+
